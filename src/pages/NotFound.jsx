0340e0baa2b7b3f1a0bbec707e0ff0572175e33f
Santiago Paolantonio <riossantiago111@gmail.com>
2025-10-26 15:24:14 -0300

    Arreglo y modificaciones en estilos

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,16 +3,21 @@ import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-gray-950 via-gray-900 to-gray-800 text-center text-white p-6">
-      <h1 className="text-[8rem] md:text-[10rem] font-extrabold tracking-tight text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-purple-600 drop-shadow-[0_0_25px_rgba(139,92,246,0.8)]">
+    <div
+      className="min-h-screen flex flex-col items-center justify-center text-center 
+                    bg-gray-300 border border-gray-200 shadow-xs rounded-lg bg-linear-to-b 
+                    dark:bg-gradient-to-b dark:from-gray-950 dark:via-gray-900 dark:to-gray-800 
+                    text-gray-800 dark:text-gray-100 transition-colors duration-500"
+    >
+      <h1 className="text-[6rem] sm:text-[8rem] md:text-[10rem] font-extrabold tracking-tight text-transparent bg-clip-text bg-gradient-to-b from-fuchsia-500 to-purple-600  dark:from-blue-500 to-purple-600 drop-shadow-[0_0_25px_rgba(139,92,246,0.8)]">
         404
       </h1>
 
-      <p className="text-2xl md:text-3xl font-semibold mt-4 text-gray-200">
+      <p className="text-2xl md:text-3xl font-semibold mt-4 text-gray-800 dark:text-white">
         Ups... página no encontrada
       </p>
 
-      <p className="text-gray-400 mt-2 mb-8 max-w-md">
+      <p className="text-gray-800 dark:text-white mt-2 mb-8 max-w-md">
         Parece que te perdiste entre los acordes 🎵. No te preocupes, podés volver al inicio.
       </p>
 
