489363b7625bc0b7bdb43b1920a5f8e5ac5f5e1a
Santiago Paolantonio <riossantiago111@gmail.com>
2025-10-28 21:28:36 -0300

    Cambio de fondo

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -3,35 +3,39 @@ import { Link } from "react-router-dom";
 
 function NotFound() {
   return (
-    <div
-      className="min-h-screen flex flex-col items-center justify-center text-center 
-                    bg-gray-300 border border-gray-200 shadow-xs rounded-lg bg-linear-to-b 
-                    dark:bg-gradient-to-b dark:from-gray-950 dark:via-gray-900 dark:to-gray-800 
-                    text-gray-800 dark:text-gray-100 transition-colors duration-500"
-    >
-      <h1 className="text-[6rem] sm:text-[8rem] md:text-[10rem] font-extrabold tracking-tight text-transparent bg-clip-text bg-gradient-to-b from-fuchsia-500 to-purple-600  dark:from-blue-500 to-purple-600 drop-shadow-[0_0_25px_rgba(139,92,246,0.8)]">
-        404
-      </h1>
+    <div className="relative min-h-screen">
+      {/* Fondo */}
+      <div
+        className="absolute inset-0 bg-cover bg-center bg-no-repeat"
+        style={{ backgroundImage: "url('/public/Fondos/Fondo-error-404.png')" }}
+      />
+      {/* Capa de contraste */}
+      <div className="absolute inset-0 bg-white/60 dark:bg-black/50" />
 
-      <p className="text-2xl md:text-3xl font-semibold mt-4 text-gray-800 dark:text-white">
-        Ups... página no encontrada
-      </p>
+      {/* Contenido */}
+      <div className="relative z-10 min-h-screen flex flex-col items-center justify-center text-center text-gray-800 dark:text-gray-100 px-4">
+        <h1 className="text-[6rem] sm:text-[8rem] md:text-[10rem] font-extrabold tracking-tight text-transparent bg-clip-text bg-gradient-to-b from-fuchsia-400 to-fuchsia-700 drop-shadow-[0_0_25px_rgba(139,92,246,0.8)]">
+          404
+        </h1>
 
-      <p className="text-gray-800 dark:text-white mt-2 mb-8 max-w-md">
-        Parece que te perdiste entre los acordes 🎵. No te preocupes, podés volver al inicio.
-      </p>
+        <p className="text-2xl md:text-3xl font-semibold mt-4">
+          Ups... página no encontrada
+        </p>
 
-      <Link
-        to="/"
-        className="px-4 py-3 text-center uppercase text-white rounded-lg shadow-lg block 
-          bg-linear-to-r from-purple-600 via-purple-700 to-purple-800 
-          hover:from-purple-700 hover:via-purple-800 hover:to-purple-900 
-          transition duration-500 ease-in-out"
-      >
-        Volver al inicio
-      </Link>
+        <p className="mt-2 mb-8 max-w-md">
+          Parece que te perdiste entre los acordes 🎵. No te preocupes, podés volver al inicio.
+        </p>
+
+        <Link
+          to="/"
+          className="px-4 py-3 text-center uppercase text-white rounded-lg shadow-lg block bg-gradient-to-r from-purple-600 via-purple-700 to-purple-800 hover:from-purple-700 hover:via-purple-800 hover:to-purple-900 transition duration-500 ease-in-out"
+        >
+          Volver al inicio
+        </Link>
+      </div>
     </div>
   );
 }
 
 export default NotFound;
+
